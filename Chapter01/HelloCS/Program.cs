8815d331d557ca944528f94515ea196f4c55b783
Ilgar Rasulov <gragli98@gmail.com>
2025-02-11 16:21:49 +0100

    test 2

--- Chapter01/HelloCS/Program.cs
+++ Chapter01/HelloCS/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // int z;
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World, again!");
+
 // throw new Exception();
