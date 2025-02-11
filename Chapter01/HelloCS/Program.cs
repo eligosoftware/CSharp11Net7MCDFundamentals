d25aab7480be8fb7d8e6f7b6b259d9d23df2f6f5
eligosoftware <gragli98@gmail.com>
2025-02-11 21:50:50 +0100

    uncommented lines

--- Chapter01/HelloCS/Program.cs
+++ Chapter01/HelloCS/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // int z;
-// Console.WriteLine("Hello, World!");
-// Console.WriteLine("Hello, World, again!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World, again!");
 
 // throw new Exception();
